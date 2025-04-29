bf075da7d8c8db3241841fbd8d5b43738f6f34a7
Salman malik <141916867+SalmanMalik01@users.noreply.github.com>
2025-04-29 12:40:33 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,27 @@
+package com.java.employeePayRole;
+
+abstract class Employee{
+	private String name;
+	private int id ;
+	
+	public Employee(String name , int id ) {
+		this.name=name;
+		this.id= id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public abstract double CalculateSalary();
+	@Override
+	
+	public String toString () {
+		return "Employee [name = "+ name +",id = "+id+" salary = "+ CalculateSalary()+"]";
+	}
+	
+}
